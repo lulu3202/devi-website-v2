9dc1db83982b3e7a98341448a08ce1f7930d2800
lulu3202 <priyamadurai2022@gmail.com>
2026-04-03 17:13:34 +0530

    Fix base path for devi-website-v2 repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://lulu3202.github.io',
-  base: '/devi',
+  base: '/devi-website-v2',
   output: 'static',
 });
